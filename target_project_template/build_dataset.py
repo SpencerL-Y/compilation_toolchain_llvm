b390ae83fa57cfae7503b423ddba93301e8b8d0a
Clexma <lixie19@ios.ac.cn>
2025-04-10 14:17:57 +0800

    add prompts

--- target_project_template/build_dataset.py
+++ target_project_template/build_dataset.py
@@ -7,7 +7,8 @@ print("here")
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_SOURCE_FOLDER = os.path.join(PROJECT_DIR, "static_file_data/src")
 TARGET_LABELLED_SRC = os.path.join(PROJECT_DIR, "files_with_label")
-TRAINING_DATA_FOLDER = os.path.join(PROJECT_DIR, "training_data")
+TRAINING_DATA_SRC_FOLDER = os.path.join(PROJECT_DIR, "training_data/src")
+TRAINING_DATA_PROMPT_FOLDER = os.path.join(PROJECT_DIR, "training_data/prompt")
 
 
 def sift_usable_cases():
@@ -45,15 +46,48 @@ def create_labelled_data():
             else:
                 accumulated_program += line
         new_file_name = os.path.splitext(os.path.basename(item))[0] + "_labelled.c"
-        nf = open(os.path.join(TRAINING_DATA_FOLDER, new_file_name),"w+")
+        nf = open(os.path.join(TRAINING_DATA_SRC_FOLDER, new_file_name),"w+")
         nf.write(accumulated_program)
         nf.close()
-        nfl = open(os.path.join(TRAINING_DATA_FOLDER, os.path.splitext(os.path.basename(item))[0] + "_labels.txt"),"w")
+        nfl = open(os.path.join(TRAINING_DATA_SRC_FOLDER, os.path.splitext(os.path.basename(item))[0] + "_labels.txt"),"w")
         for key in label_id2test_result:
             print(label_id2test_result[key])
             nfl.write("LABEL " + str(key) + "," + label_id2test_result[key][0][0] + "," + label_id2test_result[key][0][1] + "," + label_id2test_result[key][1] + "\n") 
+
+def formulating_training_prompt():
+    for item in list(Path(TRAINING_DATA_SRC_FOLDER).rglob("*.c")):
+        f = open(item, "r")
+        src_content = f.read()
+        file_base_name = os.path.splitext(os.path.basename(item))[0]
+        original_base_name = file_base_name.split("_labelled")[0]
+        labels_file_name = original_base_name + "_labels.txt"
+        labels_file_path = os.path.join(TRAINING_DATA_SRC_FOLDER, labels_file_name)
+        lf = open(labels_file_path, "r")
+        for line in lf.readlines():
+            splitted_labels = line.strip().split(",")
+            label_str = splitted_labels[0]
+            first_ptr = splitted_labels[1]
+            second_ptr = splitted_labels[2]
+            answer = splitted_labels[3]
+        
+        role_str = "Pointer analyzer that analyzes whether two pointer in a program alias"
+        question = "Analyze whether pointer expression " + first_ptr + " and pointer expression " + second_ptr + \
+                   " alias in the following program at the position with \"------- " + label_str + " \":\n"
+        question += src_content + "\n"
+        answer = "The two pointer expression " + answer + " alias\n"
+        wf_path = os.path.join(TRAINING_DATA_PROMPT_FOLDER, original_base_name + "_prompt.txt")
+        wf = open(wf_path, "w")
+        wf.write("[ROLE]\n")
+        wf.write(role_str + "\n")
+        wf.write("[QUESTION]\n")
+        wf.write(question)
+        wf.write("[ANSWER]\n")
+        wf.write(answer)
+
+
 
 if __name__ == "__main__":
     print("start")
     sift_usable_cases()
     create_labelled_data()
+    formulating_training_prompt()
